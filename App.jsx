9e37edbf25d1ef414206d63fb2160cc36a69dffa
A01Akshat <104577005+A01Akshat@users.noreply.github.com>
2023-01-06 20:23:15 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -3,25 +3,40 @@ import React, { useState } from "react";
 const App=()=>{
 
  const [name,setname]=useState("");
+ const [lname,setlname]=useState("");
  const [fname,setfname]=useState();
+ const [fnamen,setfnamen]=useState("");
 
 const eventchange=(event)=>
 {
 setname(event.target.value);
 }
 
-const sub=()=>{
+const sub=(event)=>{
+    event.preventDefault();//prevents default behaviour of form tag
     setfname(name);
+    setfnamen(lname);
 }
+
+const eventchangetwo=(event)=>{
+setlname(event.target.value);
+}
+
 
     return(
         <>
 <div>
-    <h1>Hello {fname}</h1>
+<form onSubmit={sub}>
+    <h1>Hello {fname} {fnamen}</h1>
     <input type='text' onChange={eventchange}
         value={name}
     />
-    <button onClick={sub}>CLICK ME</button>
+    <br></br>
+    <input type='text' onChange={eventchangetwo}
+        value={lname}
+    />
+    <button type="submit">CLICK ME</button>
+    </form> 
 </div>
         </>
     );
